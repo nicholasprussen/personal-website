b6ba9b2732015f5dda42d096f146a6f8b8326ee0
nicholasprussen <59841031+nicholasprussen@users.noreply.github.com>
2020-10-20 06:15:43 -0600

    first draft website with template

--- personal-site-template/src/Components/Footer.js
+++ personal-site-template/src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 Nicholas Prussen</li>
+              <li>Template by <a title="Tim Baker" href="https://github.com/tbakerx/react-resume-template">Tim Baker</a></li>
            </ul>
 
         </div>
